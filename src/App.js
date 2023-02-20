ba7c243c9770d64d00f1ab6941ea02e8453defa5
AndreaPossidente <ndrpssdnt@gmail.com>
2023-02-20 11:02:08 +0100

    done exercise 12

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
       <div>
         <Hello />
-        <Welcome name="Gianni" />
+        <Welcome name="Gianni" age={25} />
         <Welcome />
       </div>
     );
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class Welcome extends Component {
   render() {
-    return <p>Welcome, {this.props.name || "Anonymous"}!</p>;
+    return (
+      <>
+        <p>Welcome, {this.props.name || "Anonymous"}!</p>
+        <p>Your age is {this.props.age || "unknown"}</p>
+      </>
+    );
   }
 }
