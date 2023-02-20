7405fe58c37b369c1471e094011d2acd643b45c9
AndreaPossidente <ndrpssdnt@gmail.com>
2023-02-20 11:07:24 +0100

    done exercise 14

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Age from "./Age";
 
 export default class Welcome extends Component {
   render() {
     return (
       <>
         <p>Welcome, {this.props.name || "Anonymous"}!</p>
-        <p>Your age is {this.props.age || "unknown"}</p>
+        <Age age={this.props.age} />
       </>
     );
   }
