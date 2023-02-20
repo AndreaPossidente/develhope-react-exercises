11e4e951f973b330f633ac2d9221679a3b6a7069
AndreaPossidente <ndrpssdnt@gmail.com>
2023-02-20 09:06:16 +0100

    done exercise 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 function App() {
   const hello = <h1>Hello, World!</h1>;
 
-  return <div>{hello}</div>;
+  const sayHello = (name) => <h1>Hello, {name}</h1>;
+
+  return (
+    <div>
+      {hello}
+      <br />
+      {sayHello("Gianni")}
+    </div>
+  );
 }
 
 export default App;
