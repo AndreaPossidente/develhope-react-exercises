8b61e1e6e43cdd8b043b3fbe314655c348373b33
AndreaPossidente <ndrpssdnt@gmail.com>
2023-02-20 09:09:32 +0100

    done exercise 3

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ function App() {
 
   const sayHello = (name) => <h1>Hello, {name}</h1>;
 
+  const sum = (a, b) => <h2>{a + b}</h2>;
+
   return (
     <div>
       {hello}
       <br />
       {sayHello("Gianni")}
+      <br />
+      {sum(10, 5)}
     </div>
   );
 }
