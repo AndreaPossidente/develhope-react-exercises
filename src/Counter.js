9eeaae06d4f576edd728a1c570af7a9733023dfb
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-01 14:37:01 +0100

    done exercise 23

--- src/Counter.js
+++ src/Counter.js
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 import CounterDisplay from "./CounterDisplay";
 
 export default class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: this.props.initialValue };
+  state = { counter: this.props.initialValue };
 
+  componentDidMount() {
     setInterval(() => {
       this.setState((prevState) => ({
         counter: prevState.counter + this.props.increment,
