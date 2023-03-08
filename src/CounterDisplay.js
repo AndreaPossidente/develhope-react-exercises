a90a6caa4bed221780dd197b026f0b8ad374f1b3
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-08 20:18:00 +0100

    done styling components 02

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class CounterDisplay extends Component {
   render() {
-    return <h1>{this.props.counter}</h1>;
+    return (
+      <div
+        style={{
+          backgroundColor: "#acc2b2",
+          color: "#454d47",
+          width: "200px",
+          padding: "0px 5px",
+          borderRadius: "6px",
+          textAlign: "right",
+        }}
+      >
+        <h1>{this.props.counter}</h1>
+      </div>
+    );
   }
 }
