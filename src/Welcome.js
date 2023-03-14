752dcfc0a38f20cc7a4c59360f95839d36ff6e2e
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-14 20:00:49 +0100

    done exercise 43

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import Age from "./Age";
 
-export default class Welcome extends Component {
-  render() {
-    return (
-      <div className="welcome">
-        <p>Welcome, {this.props?.name || "Anonymous"}!</p>
-        {this.props?.age && <Age age={this.props.age} />}
-      </div>
-    );
-  }
+// export default class Welcome extends Component {
+//   render() {
+//     return (
+//       <div className="welcome">
+//         <p>Welcome, {this.props?.name || "Anonymous"}!</p>
+//         {this.props?.age && <Age age={this.props.age} />}
+//       </div>
+//     );
+//   }
+// }
+
+export default function Welcome({ name = "Anonymous", age }) {
+  return (
+    <div className="welcome">
+      <p>Welcome, {name}!</p>
+      {age && <Age age={age} />}
+    </div>
+  );
 }
