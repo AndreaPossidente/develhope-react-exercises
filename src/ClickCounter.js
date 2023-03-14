f40f86f42ab7e34e060ecee177c51a4d1a3c9a49
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-14 20:39:23 +0100

    done exercise 46

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,20 +1,37 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-export default class ClickCounter extends Component {
-  state = { counter: 0 };
+// export default class ClickCounter extends Component {
+//   state = { counter: 0 };
 
-  handleIncrement = () => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + 1,
-    }));
+//   handleIncrement = () => {
+//     this.setState((prevState) => ({
+//       counter: prevState.counter + 1,
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Click Count: {this.state.counter}</h1>
+//         <button onClick={this.handleIncrement}>Increment</button>
+//       </div>
+//     );
+//   }
+// }
+
+import React, { useState } from "react";
+
+export default function ClickCounter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Click Count: {this.state.counter}</h1>
-        <button onClick={this.handleIncrement}>Increment</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Click Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
 }
