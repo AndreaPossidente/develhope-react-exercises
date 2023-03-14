d8999c9b5e2cec88953a13907f86bc9cd5fff83a
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-14 22:29:16 +0100

    done exercise 50

--- src/Sum.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-// if the numbers prop is not present, i'll set a default value for it like this:
-export default function Sum({ numbers = [0] }) {
-  return <h1>{numbers.reduce((a, b) => a + b)}</h1>;
-}
