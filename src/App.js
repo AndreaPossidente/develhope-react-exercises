36ae3d0de21a33ecafcbd7b9741d36aeb5265ac3
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-14 19:37:35 +0100

    done exercise 40

--- src/App.js
+++ src/App.js
@@ -15,10 +15,42 @@ export default class App extends Component {
     console.log(user);
   };
   render() {
+    const styles = {
+      list: "border overflow-hidden rounded-md m-2 w-[346px]",
+      listItem:
+        "border-b flex justify-between p-2 even:bg-slate-100 last:border-none",
+      deleteBtn:
+        "transition ease-in-out duration-200 text-xs hover:text-red-600 hover:scale-105",
+    };
+
     return (
       <div>
         <hr />
-        <TodoList />
+        {/* START TODO LIST */}
+        <TodoList
+          render={(items, handleRemove) => {
+            return (
+              <ul
+                className={`${styles.list} ${
+                  items.length === 0 && "invisible"
+                }`}
+              >
+                {items.map((item, index) => (
+                  <li key={item + index} className={styles.listItem}>
+                    {item}
+                    <button
+                      className={styles.deleteBtn}
+                      onClick={() => handleRemove(index)}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            );
+          }}
+        />
+        {/* END TODO LIST */}
         <Hello />
         <hr />
         <Welcome name={<strong>Gianni</strong>} age={25} />
--- src/TodoList.js
+++ src/TodoList.js
@@ -26,49 +26,39 @@ export default class TodoList extends Component {
     todo.focus();
   };
 
-  handleRemove(index) {
+  handleRemove = (index) => {
     this.setState((state) => ({
       items: state.items.filter((item, itemIndex) => itemIndex !== index),
     }));
-  }
+  };
 
   render() {
+    const styles = {
+      container: "flex w-full justify-center items-center flex-col",
+      title: "text-xl font-bold px-2 mx-2",
+      input: "border py-1 px-1 m-2",
+      addBtn:
+        "transition ease-in-out duration-300 bg-slate-100 hover:bg-green-200 border py-1 px-3 m-1",
+      resetBtn:
+        "transition ease-in-out duration-300 bg-slate-100 hover:bg-yellow-200 border py-1 px-3 m-1",
+    };
+
     return (
-      <div className="flex w-full justify-center items-center flex-col">
-        <h1 className="text-xl font-bold px-2 mx-2">To Do List</h1>
+      <div className={styles.container}>
+        <h1 className={styles.title}>To Do List</h1>
         <form ref={this.formRef} onSubmit={this.handleSubmit}>
-          <input name="todo" className="border py-1 px-1 m-2" />
-          <button className="transition ease-in-out duration-300 bg-slate-100 hover:bg-green-200 border py-1 px-3 m-1 ">
-            Add
-          </button>
+          <input name="todo" className={styles.input} />
+          <button className={styles.addBtn}>Add</button>
           <button
-            className="transition ease-in-out duration-300 bg-slate-100 hover:bg-yellow-200 border py-1 px-3 m-1 "
+            className={styles.resetBtn}
             type="reset"
             onClick={this.handleReset}
           >
             Reset
           </button>
         </form>
-        <ul
-          className={`border overflow-hidden rounded-md m-2 w-[346px] ${
-            this.state.items.length === 0 && "invisible"
-          }`}
-        >
-          {this.state.items.map((item, index) => (
-            <li
-              key={item + index}
-              className="border-b flex justify-between p-2 even:bg-slate-100 last:border-none"
-            >
-              {item}{" "}
-              <button
-                className="transition ease-in-out duration-200 text-xs hover:text-red-600 hover:scale-105"
-                onClick={() => this.handleRemove(index)}
-              >
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
+        {/* RENDER PROP */}
+        {this.props.render(this.state.items, this.handleRemove)}
       </div>
     );
   }
