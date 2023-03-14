216a743bf94198b01a0e44d1df3cd473b1615241
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-14 19:55:38 +0100

    done exercise 42

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -7,7 +7,13 @@ export default class DisplayLanguage extends Component {
       <div>
         <LanguageContext.Consumer>
           {(lang) => {
-            return <div></div>;
+            return (
+              <div>
+                {lang === "it" && "Hai scelto la lingua italiana"}
+                {lang === "en" && "You have chosen the english language"}
+                {lang === "fr" && "Vous avez choisi la langue française"}
+              </div>
+            );
           }}
         </LanguageContext.Consumer>
       </div>
