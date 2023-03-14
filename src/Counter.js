1e1aac4f1a6d38bb231b532ff0e83119edd890ce
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-14 21:09:35 +0100

    done exercise 49

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,39 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import CounterDisplay from "./CounterDisplay";
+
+// export default class Counter extends Component {
+//   state = { counter: this.props.initialValue };
+
+//   componentDidMount() {
+//     setInterval(() => {
+//       this.setState((prevState) => ({
+//         counter: prevState.counter + this.props.increment,
+//       }));
+//     }, this.props.interval);
+//   }
+//   render() {
+//     return <CounterDisplay counter={this.state.counter} />;
+//   }
+// }
+
+import React, { useEffect, useState } from "react";
 import CounterDisplay from "./CounterDisplay";
 
-export default class Counter extends Component {
-  state = { counter: this.props.initialValue };
+export default function Counter({
+  initialValue = 0,
+  increment = 1,
+  interval = 1000,
+}) {
+  const [count, setCount] = useState(initialValue);
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState((prevState) => ({
-        counter: prevState.counter + this.props.increment,
-      }));
-    }, this.props.interval);
-  }
-  render() {
-    return <CounterDisplay counter={this.state.counter} />;
-  }
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount((prevCount) => prevCount + increment);
+    }, interval);
+    return () => {
+      return clearInterval(intervalId);
+    };
+  }, [increment, interval]);
+
+  return <CounterDisplay counter={count} />;
 }
