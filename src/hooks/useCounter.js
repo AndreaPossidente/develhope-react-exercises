47ee82066ca680beaadacf171ee463f1318ed3dd
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-20 14:40:56 +0100

    done exercise 58

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function useCounter(initialValue = 0) {
   const [counter, setCounter] = useState(initialValue);
 
-  const handleIncrement = () => {
+  const handleIncrement = useCallback(() => {
     setCounter((counter) => counter + 1);
-  };
-  const handleDecrement = () => {
+  }, []);
+
+  const handleDecrement = useCallback(() => {
     setCounter((counter) => counter - 1);
-  };
-  const handleReset = () => {
+  }, []);
+
+  const handleReset = useCallback(() => {
     setCounter(initialValue);
-  };
+  }, [initialValue]);
 
   return {
     counter,
