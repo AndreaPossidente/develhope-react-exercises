e54d487e2c145a5097677cd43919f8d9005eab85
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-20 14:37:35 +0100

    done exercise 57

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,22 +1,36 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import { LanguageContext } from "./LanguageContext";
+
+// export default class DisplayLanguage extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <LanguageContext.Consumer>
+//           {(lang) => {
+//             return (
+//               <div>
+//                 {lang === "it" && "Hai scelto la lingua italiana"}
+//                 {lang === "en" && "You have chosen the english language"}
+//                 {lang === "fr" && "Vous avez choisi la langue française"}
+//               </div>
+//             );
+//           }}
+//         </LanguageContext.Consumer>
+//       </div>
+//     );
+//   }
+// }
+
+import React, { useContext } from "react";
 import { LanguageContext } from "./LanguageContext";
 
-export default class DisplayLanguage extends Component {
-  render() {
-    return (
-      <div>
-        <LanguageContext.Consumer>
-          {(lang) => {
-            return (
-              <div>
-                {lang === "it" && "Hai scelto la lingua italiana"}
-                {lang === "en" && "You have chosen the english language"}
-                {lang === "fr" && "Vous avez choisi la langue française"}
-              </div>
-            );
-          }}
-        </LanguageContext.Consumer>
-      </div>
-    );
-  }
+export default function DisplayLanguage() {
+  const lang = useContext(LanguageContext);
+  return (
+    <div>
+      {lang === "it" && "Hai scelto la lingua italiana"}
+      {lang === "en" && "You have chosen the english language"}
+      {lang === "fr" && "Vous avez choisi la langue française"}
+    </div>
+  );
 }
