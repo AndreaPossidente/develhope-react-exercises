5400320d7128544dabb299c74b281ecbaa0117bb
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-20 15:35:39 +0100

    done exercise 63

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Counter from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
 import Welcome from "./Welcome";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome name="Andrea" />} />
-      <Route
-        path="/counter"
-        element={<Counter initialValue={0} increment={1} interval={1000} />}
-      />
-      <Route path="/users/:username" element={<ShowGithubUser />} />
-    </Routes>
+    <div>
+      <Link to="/">Welcome</Link> | <Link to="/counter">Counter</Link> |{" "}
+      <Link to="/users/AndreaPossidente">Users: AndreaPossidente</Link>
+      <Routes>
+        <Route path="/" element={<Welcome name="Andrea" />} />
+        <Route
+          path="/counter"
+          element={<Counter initialValue={0} increment={1} interval={1000} />}
+        />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
+      </Routes>
+    </div>
   );
 }
