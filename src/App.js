f538384e1a3b7a53bc58971e114faee09357e1b1
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-20 15:24:01 +0100

    done exercise 61

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Counter from "./Counter";
 import Welcome from "./Welcome";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome name="Andrea" />} />
+      <Route
+        path="/counter"
+        element={<Counter initialValue={0} increment={1} interval={1000} />}
+      />
     </Routes>
   );
 }
