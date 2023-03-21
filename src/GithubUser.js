d0f910f3b33fcc0a1f66583b3761731d2e72bfa1
AndreaPossidente <ndrpssdnt@gmail.com>
2023-03-21 11:46:49 +0100

    done exercise 68

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -5,16 +5,14 @@ import useGithubUser from "./hooks/useGithubUser";
 and renders some of the data within a div tag. The API to query is https://api.github.com/users/${username}.
  */
 export default function GithubUser({ username }) {
-  const { user, error, loading } = useGithubUser(username);
+  //const { user, error, loading } = useGithubUser(username);
+  // ho tolto l'username per far vedere che se non c'è non viene effettuata una chiamata all'api grazie al conditional fetching di SWR
+  const { user, error, loading } = useGithubUser();
   return (
     <div>
       {loading && <div className="animate-ping">loading...</div>}
-      {error && (
-        <div className="border rounded-lg m-3 p-1 text-center max-w-[250px] overflow-hidden">
-          {error.message}
-        </div>
-      )}
-      {user && !error && (
+      {error && <h2>{error}</h2>}
+      {user && (
         <div className="border rounded-lg m-3 max-w-[250px] overflow-hidden">
           <img src={user.avatar_url} alt={user.name} />
           <div className="flex flex-col items-center p-3">
--- src/hooks/useGithubUser.js
+++ src/hooks/useGithubUser.js
@@ -4,8 +4,15 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function useGithubUser(username) {
   const { data, error, isLoading } = useSWR(
-    `https://api.github.com/users/${username}`,
-    fetcher
+    username ? `https://api.github.com/users/${username}` : null,
+    fetcher,
+    {
+      onError: {
+        data: null,
+        error: "Impossibile trovare l'utente",
+        isLoading: false,
+      },
+    }
   );
   return { user: data, error, loading: isLoading };
 }
